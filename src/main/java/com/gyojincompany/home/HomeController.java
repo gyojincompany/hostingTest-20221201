7c62fda03bf468023dc8337201926b334cca731d
gyojincompany <luckyred@gmail.com>
2022-12-01 09:25:46 +0900

    롬복 테스트 추가

--- src/main/java/com/gyojincompany/home/HomeController.java
+++ src/main/java/com/gyojincompany/home/HomeController.java
@@ -1,6 +1,7 @@
 package com.gyojincompany.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +18,17 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/content")
-	public String content() {
+	public String content(Model model) {
+		
+		TestDto dto1 = new TestDto("Kim", 27);
+		
+		TestDto dto2 = new TestDto();
+		dto2.setName("hong");
+		dto2.setAge(32);
+		
+		model.addAttribute("dto1", dto1);
+		model.addAttribute("dto2", dto2);
+		
 		return "content";
 	}
 	
---
+++ src/main/java/com/gyojincompany/home/TestDto.java
@@ -0,0 +1,14 @@
+package com.gyojincompany.home;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestDto {
+	
+	private String name;
+	private int age;
+}
